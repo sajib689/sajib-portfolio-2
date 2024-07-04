57d6efb3def3adf0b785b5cfdfdcc778437504f4
sajib689 <jibonraja11@gmail.com>
2024-07-04 18:08:15 +0600

    chnage on footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <div className="flex flex-col">
+      <div className="container text-center lg:flex md:flex flex-row p-12 justify-between">
+        <div className="flex flex-col text-slate-400">
         <span>Email: sajibbabu751@gmail.com</span>
         <span>Phone: +88016-11970979</span>
         </div>
+        <div>
         <p className="text-slate-600">All rights reserved by Sajib.</p>
+        </div>
       </div>
     </footer>
   );
