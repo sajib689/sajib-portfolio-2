b465c263b0254585d6014392c8ac97a97659ed63
sajib689 <jibonraja11@gmail.com>
2025-02-10 20:50:08 +0600

    update HeroSection link to new Google Drive URL

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
             </Link>
             <Link
               target="_blank"
-              href="https://drive.google.com/file/d/1oQ-hGbpV-KCFBGLK1hyouzA_wnWzOcRb/view?usp=sharing"
+              href="https://drive.google.com/file/d/1ZkcILMmXHDpmZx3es0ij1isk4FLYodLw/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
