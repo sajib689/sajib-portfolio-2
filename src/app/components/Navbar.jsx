79722a95f6ead46abe37ee9b90214d5755ff7952
sajib689 <jibonraja11@gmail.com>
2025-02-11 11:13:16 +0600

    add NavLink component and update Navbar to include icons for navigation links

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,22 +7,76 @@ import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
+    title: "Home",
+    path: "/",
+    icon: (
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 576 512"
+        height="1em"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-5 h-5"
+      >
+        <path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path>
+      </svg>
+    ),
+  },
+  {
     title: "About",
     path: "#about",
+    icon: (<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>)
+  },
+  {
+    title: "Skills",
+    path: "#skills",
+    icon: (<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 640 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M622.34 153.2L343.4 67.5c-15.2-4.67-31.6-4.67-46.79 0L17.66 153.2c-23.54 7.23-23.54 38.36 0 45.59l48.63 14.94c-10.67 13.19-17.23 29.28-17.88 46.9C38.78 266.15 32 276.11 32 288c0 10.78 5.68 19.85 13.86 25.65L20.33 428.53C18.11 438.52 25.71 448 35.94 448h56.11c10.24 0 17.84-9.48 15.62-19.47L82.14 313.65C90.32 307.85 96 298.78 96 288c0-11.57-6.47-21.25-15.66-26.87.76-15.02 8.44-28.3 20.69-36.72L296.6 284.5c9.06 2.78 26.44 6.25 46.79 0l278.95-85.7c23.55-7.24 23.55-38.36 0-45.6zM352.79 315.09c-28.53 8.76-52.84 3.92-65.59 0l-145.02-44.55L128 384c0 35.35 85.96 64 192 64s192-28.65 192-64l-14.18-113.47-145.03 44.56z"></path></svg>)
   },
   {
     title: "Projects",
     path: "#projects",
+    icon: (<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 640 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M384 320H256c-17.67 0-32 14.33-32 32v128c0 17.67 14.33 32 32 32h128c17.67 0 32-14.33 32-32V352c0-17.67-14.33-32-32-32zM192 32c0-17.67-14.33-32-32-32H32C14.33 0 0 14.33 0 32v128c0 17.67 14.33 32 32 32h95.72l73.16 128.04C211.98 300.98 232.4 288 256 288h.28L192 175.51V128h224V64H192V32zM608 0H480c-17.67 0-32 14.33-32 32v128c0 17.67 14.33 32 32 32h128c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32z"></path></svg>)
   },
   {
     title: "Contact",
     path: "#contact",
+    icon: (
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 512 512"
+        height="1em"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-5 h-5"
+      >
+        <path d="M502.3 190.8c3.9-3.2 6.3-8 6.3-13.1s-2.3-9.9-6.3-13.1L272 3.4c-7.7-5.6-17.6-5.6-25.3 0L9.7 164.5C5.7 167.7 3.3 172.5 3.3 177.6s2.4 9.9 6.3 13.1L208 326.6V464h96V326.6L502.3 190.8zM464 448H48c-26.5 0-48 21.5-48 48s21.5 48 48 48h416c26.5 0 48-21.5 48-48s-21.5-48-48-48z"></path>
+      </svg>
+    ),
   },
+  
   {
     title: "Login",
     path: "/login",
+    icon: (<svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 512 512"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-5 h-5"
+    >
+      <path d="M192 128c0-17.7 14.3-32 32-32h128c17.7 0 32 14.3 32 32v256c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32v-64h-32v64c0 35.3 28.7 64 64 64h128c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64v64h32v-64zM352 272H160v32h192v-32zM273 216.1l39-39c6.3-6.3 6.3-16.4 0-22.6-6.3-6.3-16.4-6.3-22.6 0l-67 67c-6.3 6.3-6.3 16.4 0 22.6l67 67c6.3 6.3 16.4 6.3 22.6 0 6.3-6.3 6.3-16.4 0-22.6l-39-39H352v-32H273z"></path>
+    </svg>
+    )
   },
 ];
+
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
@@ -54,13 +108,14 @@ const Navbar = () => {
           )}
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-          </ul>
+        <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+  {navLinks.map((link, index) => (
+    <li key={index}>
+      <NavLink href={link.path} title={link.title} icon={link.icon} />
+    </li>
+  ))}
+</ul>
+
         </div>
       </div>
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
