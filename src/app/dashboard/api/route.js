afb0ec6a2bb841e4784ea88c4feed309603d7979
sajib689 <jibonraja11@gmail.com>
2025-02-14 17:50:42 +0600

    add API route for fetching projects from the database

---
+++ src/app/dashboard/api/route.js
@@ -0,0 +1,13 @@
+import connectionDb from "@/DB/ConnectDb";
+
+
+export default async function handler(req, res) {
+    try {
+        const client = await connectionDb;
+        const db = client.db("portfolio");
+        const data = await db.collection("projects").inertOne()
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Database connection failed" });
+    }
+}
